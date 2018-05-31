fc16717b0fa96366c00aaaab47d81230984e3838
yamazaki-mitsufumi <yamazaki-mitsufumi-mn@ynu.jp>
2018-06-01 02:17:56 +0900

    [fix] done

--- BN254pairing/unterluggauer_cpa.py
+++ BN254pairing/unterluggauer_cpa.py
@@ -1,6 +1,5 @@
 import numpy as np
 import os
-import time
 from scipy.stats import pearsonr
 from multiprocessing import Pool
 
@@ -9,11 +8,33 @@ from multiprocessing import Pool
 
 def get_parameter(request):
     parameter = {}
-    parameter["known_words"] = 0
+    parameter["poi_start"] = 0
+    parameter["poi_end"] = 10
     parameter["target_word"] = 0
     parameter["word_length"] = 8
     parameter["modulo"] = 0x100
-    parameter["output_format"] = '02x'
+    parameter["k"] = 16
+    parameter["wave_dir_name"] = 'wave'
+    parameter["plain_text_file_name"] = 'plain_cipher.txt'
+    parameter["result_file_name"] = 'result.txt'
+    parameter["output_dir_name"] = 'unterluggauer_cpa_out'
+    parameter["word_format"] = '02x'
+
+    target_word = parameter["target_word"]
+
+    output_candidate_key_length = 2*target_word+2
+    output_candidate_key_format = '0' + str(output_candidate_key_length) + 'x'
+    parameter["output_candidate_key_format"] = output_candidate_key_format
+
+    output_dir_length = 2*target_word if target_word != 0 else 2
+    output_dir_format = '0' + str(output_dir_length) + 'x'
+    parameter["output_dir_format"] = output_dir_format
+
+    input_candidate_file_name = 'candidate_on_aiming_'+str(target_word)+'word.txt'
+    parameter["input_candidate_file_name"] = input_candidate_file_name
+
+    output_candidate_file_name = 'candidate_on_aiming_'+str(target_word+1)+'word.txt'
+    parameter["output_candidate_file_name"] = output_candidate_file_name
     return parameter[request]
 
 def get_waves(wave_dir):
@@ -23,16 +44,16 @@ def get_waves(wave_dir):
         wave = []
         file_path = os.path.join(wave_dir, file_name)
         with open(file_path) as file:
-            raw = file.read().split()	#this is str
-            wave = list(map(int,raw))
+            str_wave = file.read().split()
+            wave = list(map(int,str_wave))
         waves.append(wave)
     return waves
 
 def get_plain_paragraph(plain_text_file):
     plain_paragraph = []
     with open(plain_text_file) as file:
-        for raw in file:
-            plain_text = int(raw.split(',')[1],16)	#wavefile,plaintext,cihpertext
+        for row in file:
+            plain_text = int(row.split(',')[1],16)	#wavefile,plaintext,cihpertext
             plain_paragraph.append(plain_text)
     return plain_paragraph
 
@@ -56,8 +77,7 @@ def calculate_unterluggauer_hamming_weight(plain_matrix,key_vector):
         key_hamming_weight_vector = []
         value = 0
         for m in range(l):
-            i = m+1
-            value = value + (np.dot(plain_vector[:i],key_vector[l-i:]) >> ((l-m)*word_length))
+            value = value + (np.dot(plain_vector[:m+1],key_vector[l-m:]) >> ((l-m)*word_length))
         for key in range(2**word_length):
             key_vector[0] = key
             c = int(value + np.dot(plain_vector,key_vector)) % modulo
@@ -76,27 +96,61 @@ def change_0_to_randint_with(matrix):
 def calculate_correlation(left_matrix,right_matrix):
     matrix = []
     for l in left_matrix:
-        raw = []
+        row = []
         for r in right_matrix:
             correlation, p = pearsonr(l,r)
-            raw.append(correlation)
-        matrix.append(raw)
+            row.append(correlation)
+        matrix.append(row)
     return matrix
 
-def cpa(known_key):
-    T_Start = 0
-    T_End = 10
+def output_correlation(candidate_key, correlation_matrix):
+    output_dir_name = get_parameter("output_dir_name")
+    output_dir_format = get_parameter("output_dir_format")
+    word_format = get_parameter("word_format")
+    output_dir = os.path.join(os.getcwd(), output_dir_name)
+    key_dir = os.path.join(output_dir, format(candidate_key, output_dir_format))
+    if not os.path.isdir(key_dir):
+        os.mkdir(key_dir)
 
-    wave_dir = os.path.join(os.getcwd(),'wave')
+    for key,correlation in enumerate(correlation_matrix):
+        output_file = os.path.join(key_dir, format(key, word_format))
+        with open(output_file, 'w') as file:
+            str_correlation = list(map(str, correlation))
+            file.write('\n'.join(str_correlation))
+
+def output_max_correlation(candidate_key, subscripted_max_correlation_vector):
+    output_dir_name = get_parameter("output_dir_name")
+    output_dir_format = get_parameter("output_dir_format")
+    word_format = get_parameter("word_format")
+    output_dir = os.path.join(os.getcwd(),output_dir_name)
+    key_dir = os.path.join(output_dir,format(candidate_key,output_dir_format))
+    if not os.path.isdir(key_dir):
+        os.mkdir(key_dir)
+
+    result_file_name = get_parameter("result_file_name")
+    output_file = os.path.join(key_dir,result_file_name)
+    with open(output_file, 'w') as file:
+        for key_max in subscripted_max_correlation_vector:
+            key = format(key_max[0], word_format)
+            max_correlation = str(key_max[1])
+            file.write(key+','+max_correlation+'\n')
+
+def cpa(candidate_key):
+    poi_start = get_parameter("poi_start")
+    poi_end = get_parameter("poi_end")
+
+    wave_dir_name = get_parameter("wave_dir_name")
+    wave_dir = os.path.join(os.getcwd(),wave_dir_name)
     slowly_wave_matrix = get_waves(wave_dir)
-    wave_matrix = np.array(slowly_wave_matrix)[:,T_Start:T_End].T
+    wave_matrix = np.array(slowly_wave_matrix)[:,poi_start:poi_end].T
 
-    plain_text_file = os.path.join(os.getcwd(),'plain_cipher.txt')
+    plain_text_file_name = get_parameter("plain_text_file_name")
+    plain_text_file = os.path.join(os.getcwd(),plain_text_file_name)
     plain_paragraph = get_plain_paragraph(plain_text_file)
     slowly_plain_matrix = list(map(vector_transformed_by, plain_paragraph))
     plain_matrix = np.array(slowly_plain_matrix)
 
-    slowly_key_vector = vector_transformed_by(known_key)[::-1]
+    slowly_key_vector = vector_transformed_by(candidate_key)[::-1]
     key_vector = np.array(slowly_key_vector)
 
     slowly_hamming_weight_matrix = calculate_unterluggauer_hamming_weight(plain_matrix,key_vector)
@@ -106,65 +160,74 @@ def cpa(known_key):
     correlation_matrix = calculate_correlation(hamming_weight_matrix,wave_matrix)
     max_correlation_vector = list(map(lambda key_correlation_wave: np.amax(np.absolute(key_correlation_wave)), correlation_matrix))
     subscripted_max_correlation_vector = [(key, max_correlation) for key,max_correlation in enumerate(max_correlation_vector)]
-    subscripted_max_correlation_vector.sort(key= lambda x: x[1],reverse=True)	#x[1] is correlation
+    subscripted_max_correlation_vector.sort(key= lambda key_correlation: key_correlation[1],reverse=True)
 
-    out_dir = os.path.join(os.getcwd(),"unterluggauer_cpa_out")
-    output_format = get_parameter("output_format")
-    key_dir = os.path.join(out_dir,format(known_key,"02x"))
-    if not os.path.isdir(key_dir):
-        os.mkdir(key_dir)
-    for key in range(len(correlation_matrix)):
-        out = os.path.join(key_dir,format(key,"02x"))
-        with open(out,'w') as file:
-            for t in range(len(correlation_matrix[key])):
-                file.write(str(correlation_matrix[key][t]) + '\n')
-        out = os.path.join(key_dir,'meta.txt')
-        with open(out, 'w') as file:
-            for i in range(len(correlation_matrix)):
-                file.write(str(i+1)+','+hex(subscripted_max_correlation_vector[i][0])+','+str(subscripted_max_correlation_vector[i][1]) + '\n')
+    output_correlation(candidate_key, correlation_matrix)
+    output_max_correlation(candidate_key, subscripted_max_correlation_vector)
 
+def get_candidate_key_list(file_path):
+    candidate_key_list = []
+    with open(candidate_file) as file:
+        for row in file:
+            candidate_key = int(row.split(",")[0],16)
+            candidate_key_list.append(candidate_key)
+    return candidate_key_list
+
+def get_result(candidate_key_list):
+    output_dir_name = get_parameter("output_dir_name")
+    output_dir_format = get_parameter("output_dir_format")
+    result_file_name = get_parameter("result_file_name")
+    target_word = get_parameter("target_word")
+    word_length = get_parameter("word_length")
+    output_dir = os.path.join(os.getcwd(),output_dir_name)
+
+    result = []
+    for candidate_key in candidate_key_list:
+        key_dir = os.path.join(output_dir,format(candidate_key,output_dir_format))
+        result_file = os.path.join(key_dir,result_file_name)
+        with open(result_file) as file:
+            for row in file:
+                correlation = float(row.split(',')[1])
+                key = int(row.split(',')[0],16)
+                all_key = (key << target_word*word_length) | candidate_key
+                result.append((all_key,correlation))
+    return result
+
+def output_candidate_key_list(result):
+    output_dir_name = get_parameter("output_dir_name")
+    output_dir = os.path.join(os.getcwd(),output_dir_name)
+    output_candidate_file_name = get_parameter("output_candidate_file_name")
+    output_candidate_file = os.path.join(output_dir,output_candidate_file_name)
+    result.sort(key=lambda key_correlation: key_correlation[1],reverse=True)
+    k = get_parameter("k")
+    candidate_list = result[:k]
+
+    with open(output_candidate_file,'w') as file:
+        for key_max in candidate_list:
+            output_candidate_key_format = get_parameter("output_candidate_key_format")
+            key = format(key_max[0], output_candidate_key_format)
+            max_correlation = str(key_max[1])
+            file.write(key+','+max_correlation+'\n')
 
 if __name__ == '__main__':
-    alpha_out = 16
-    known_words = get_parameter("known_words")
-    word_bits = get_parameter("word_length")
-    alpha_in_file = "alpha" + str(known_words*word_bits) + ".txt"
+    #out directory initialization
+    output_dir_name = get_parameter("output_dir_name")
+    output_dir = os.path.join(os.getcwd(),output_dir_name)
+    if not os.path.isdir(output_dir):
+        os.mkdir(output_dir)
+        input_candidate_file_name = get_parameter("input_candidate_file_name")
+        candidate_file = os.path.join(output_dir,input_candidate_file_name)
+        with open(candidate_file,"w") as file:
+            file.write("00,0.0")
 
-    out_dir = os.path.join(os.getcwd(),"unterluggauer_cpa_out")
-    if not os.path.isdir(out_dir):
-        os.mkdir(out_dir)
-        alpha_file_path = os.path.join(out_dir,alpha_in_file)
-        with open(alpha_file_path,"w") as f:
-            f.write("1,0x0,0.0")
+    input_candidate_file_name = get_parameter("input_candidate_file_name")
+    candidate_file = os.path.join(output_dir,input_candidate_file_name)
+    candidate_key_list = get_candidate_key_list(candidate_file)
 
-    alpha_keys = []
-    p = os.path.join(out_dir,alpha_in_file)
-    with open(p) as file:
-        alpha_key = 0
-        for raw in file.read().split():
-            alpha_key = int(raw.split(",")[1],16)
-            alpha_keys.append(alpha_key)
+    pool = Pool(8)
+    pool.map(cpa, candidate_key_list)
+    pool.close()
+    #cpa(candidate_key_list)
 
-    start = time.time()
-    #pool = Pool(8)
-    #pool.map(cpa, alpha_keys)
-    #pool.close()
-    cpa(0)
-    print(str(time.time() - start))
-
-    alpha_top = []
-    for alpha_key in alpha_keys:
-        key_dir = os.path.join(out_dir,format(alpha_key,"02x"))
-        meta_file = os.path.join(key_dir,"meta.txt")
-        with open(meta_file) as file:
-            for raw in file.read().split():
-                key = (int(raw.split(',')[1],16) << (known_words*word_bits) ) | alpha_key
-                r = float(raw.split(',')[2])
-                alpha_top.append((key,r))
-    alpha_top.sort(key=lambda x: x[1],reverse=True)
-
-    alpha_out_file = "alpha" + str((known_words+1)*word_bits) + ".txt"
-    p = os.path.join(out_dir, alpha_out_file)
-    with open(p,"w") as file:
-        for i in range(alpha_out):
-            file.write(str(i+1)+','+hex(alpha_top[i][0])+','+str(alpha_top[i][1]) + '\n')
+    result = get_result(candidate_key_list)
+    output_candidate_key_list(result)
