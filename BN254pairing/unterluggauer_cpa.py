e6fca3d88346147efddc17f460082e9b1188bceb
yamazaki-mitsufumi <yamazaki-mitsufumi-mn@ynu.jp>
2018-05-31 23:26:18 +0900

    [fix]remove some print function

--- BN254pairing/unterluggauer_cpa.py
+++ BN254pairing/unterluggauer_cpa.py
@@ -1,9 +1,8 @@
-import random
-import os
 import numpy as np
+import os
+import time
 from scipy.stats import pearsonr
 from multiprocessing import Pool
-import time
 
 
 #correct_key = 0x15eb62e02fbc3981f9cfed3c30c7f70c99189bed20001f2ac2a9f68e62b8da62
@@ -11,6 +10,7 @@ import time
 def get_parameter(request):
     parameter = {}
     parameter["known_words"] = 0
+    parameter["target_word"] = 0
     parameter["word_length"] = 8
     parameter["modulo"] = 0x100
     parameter["output_format"] = '02x'
@@ -19,31 +19,29 @@ def get_parameter(request):
 def get_waves(wave_dir):
     waves = []
     wave_file_list = os.listdir(wave_dir)
-    print(len(wave_file_list))
     for file_name in wave_file_list:
         wave = []
         file_path = os.path.join(wave_dir, file_name)
         with open(file_path) as file:
-            raw = file.read().split()
+            raw = file.read().split()	#this is str
             wave = list(map(int,raw))
         waves.append(wave)
-    print(len(waves))
     return waves
 
-def get_plain_text(plain_text_file):
+def get_plain_paragraph(plain_text_file):
     plain_paragraph = []
     with open(plain_text_file) as file:
         for raw in file:
-            plain_text = int(raw.split(',')[1],16)
+            plain_text = int(raw.split(',')[1],16)	#wavefile,plaintext,cihpertext
             plain_paragraph.append(plain_text)
     return plain_paragraph
 
 def vector_transformed_by(data):
     word_length = get_parameter("word_length")
     modulo = get_parameter("modulo")
-    known_words = get_parameter("known_words")
+    target_word = get_parameter("target_word")
     vector = []
-    for i in range(known_words + 1):
+    for i in range(target_word + 1):	#i = 0,1,...,target_word
         element = int(data) % modulo
         vector.append(element)
         data >> word_length
@@ -52,10 +50,10 @@ def vector_transformed_by(data):
 def calculate_unterluggauer_hamming_weight(plain_matrix,key_vector):
     word_length = get_parameter("word_length")
     modulo = get_parameter("modulo")
-    l = get_parameter("known_words")
-    hamming_weight = []
+    l = get_parameter("target_word")
+    hamming_weight_matrix = []
     for plain_vector in plain_matrix:
-        key_hamming_weight = []
+        key_hamming_weight_vector = []
         value = 0
         for m in range(l):
             i = m+1
@@ -64,9 +62,9 @@ def calculate_unterluggauer_hamming_weight(plain_matrix,key_vector):
             key_vector[0] = key
             c = int(value + np.dot(plain_vector,key_vector)) % modulo
             hw = bin(c).count('1')
-            key_hamming_weight.append(hw)
-        hamming_weight.append(key_hamming_weight)
-    return hamming_weight
+            key_hamming_weight_vector.append(hw)
+        hamming_weight_matrix.append(key_hamming_weight_vector)
+    return hamming_weight_matrix
 
 def change_0_to_randint_with(matrix):
     max_random = get_parameter("word_length")
@@ -89,39 +87,26 @@ def cpa(known_key):
     T_Start = 0
     T_End = 10
 
-    print("start")
-
     wave_dir = os.path.join(os.getcwd(),'wave')
-    wave_list_data = get_waves(wave_dir)
-    wave_matrix = np.array(wave_list_data)[:,T_Start:T_End].T
-    print(len(wave_matrix))
-
-    print("ok_w")
+    slowly_wave_matrix = get_waves(wave_dir)
+    wave_matrix = np.array(slowly_wave_matrix)[:,T_Start:T_End].T
 
     plain_text_file = os.path.join(os.getcwd(),'plain_cipher.txt')
-    plain_paragraph = get_plain_text(plain_text_file)
-    plain_matrix_data = list(map(vector_transformed_by, plain_paragraph))
-    plain_matrix = np.array(plain_matrix_data)
+    plain_paragraph = get_plain_paragraph(plain_text_file)
+    slowly_plain_matrix = list(map(vector_transformed_by, plain_paragraph))
+    plain_matrix = np.array(slowly_plain_matrix)
 
-    print("ok_p")
+    slowly_key_vector = vector_transformed_by(known_key)[::-1]
+    key_vector = np.array(slowly_key_vector)
 
-    known_key_vector_data = vector_transformed_by(known_key)[::-1]
-    key_vector = np.array(known_key_vector_data)
-
-    print("ok_k")
-
-    hamming_weight_matrix_data = calculate_unterluggauer_hamming_weight(plain_matrix,key_vector)
-    unsafe_hamming_weight_matrix = np.array(hamming_weight_matrix_data).T	#this have possibility of 0 div
+    slowly_hamming_weight_matrix = calculate_unterluggauer_hamming_weight(plain_matrix,key_vector)
+    unsafe_hamming_weight_matrix = np.array(slowly_hamming_weight_matrix).T	#this have possibility of 0 div
     hamming_weight_matrix = change_0_to_randint_with(unsafe_hamming_weight_matrix)
-
-    print("ok_h")
 
     correlation_matrix = calculate_correlation(hamming_weight_matrix,wave_matrix)
     max_correlation_vector = list(map(lambda key_correlation_wave: np.amax(np.absolute(key_correlation_wave)), correlation_matrix))
-    subscripted_max_correlation_vector = [(i, max_correlation) for i,max_correlation in enumerate(max_correlation_vector)]
-    subscripted_max_correlation_vector.sort(key= lambda x: x[1],reverse=True)
-
-    print("ok_c")
+    subscripted_max_correlation_vector = [(key, max_correlation) for key,max_correlation in enumerate(max_correlation_vector)]
+    subscripted_max_correlation_vector.sort(key= lambda x: x[1],reverse=True)	#x[1] is correlation
 
     out_dir = os.path.join(os.getcwd(),"unterluggauer_cpa_out")
     output_format = get_parameter("output_format")
@@ -177,7 +162,6 @@ if __name__ == '__main__':
                 r = float(raw.split(',')[2])
                 alpha_top.append((key,r))
     alpha_top.sort(key=lambda x: x[1],reverse=True)
-
 
     alpha_out_file = "alpha" + str((known_words+1)*word_bits) + ".txt"
     p = os.path.join(out_dir, alpha_out_file)
